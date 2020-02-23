ee76d5f423556e1ef61e0d260b460ad18d279a73
Robotboyx <vuthy.v@ligercambodia.org>
2020-02-23 12:40:23 +0700

    Heylowo

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, send_file, request, render_template, redirect, url_for
+from bson.objectid import ObjectId
 import pymongo
 import json
 import image
+import sys
+
+
 
 mongolab_uri="mongodb://admin:liger72724@liger0-shard-00-00-gyl2h.mongodb.net:27017,liger0-shard-00-01-gyl2h.mongodb.net:27017,liger0-shard-00-02-gyl2h.mongodb.net:27017/test?ssl=true&replicaSet=Liger0-shard-0&authSource=admin&retryWrites=true&w=majority"
 
@@ -21,17 +25,14 @@ def index():
         nationality = request.form['nationality']
     
     length = collection.count()
-    uniqueID = length + 1
-    userInfo = { "name": name, "nationality": nationality, "uniqueID": uniqueID }
+    order = length + 1
+    userInfo = { "name": name, "nationality": nationality, "order": order }
     y = collection.find({"name": name})
     print(y)
-    x = collection.insert_one(userInfo)
-    print(x)
-    name = name.upper()
-    nationality = nationality.upper()
-    imageName = image(name,nationality,uniqueID) 
-
-    return redirect(url_for('ticket', name=name))
+    _id = collection.insert(userInfo)
+    
+    
+    return redirect(url_for('ticket', id=_id))
 #  return send_file(name)
 
 
@@ -41,8 +42,27 @@ def name():
 
 @app.route('/ticket')
 def ticket():
-    imageName = request.args['name'] 
-    return render_template('ticket.html', image=imageName)
+    _id = request.args['id'] 
+    user = collection.find({"_id": ObjectId(_id)})
+ 
+    name = user[0]["name"].upper()
+    nationality = user[0]["nationality"].upper()
+    order = user[0]["order"]
+
+    print(user[0]["name"], file=sys.stderr)
+
+
+    imageName = image(name,nationality,order) 
+    print(imageName, file=sys.stderr)
+
+    return render_template('ticket.html', image=imageName, name=name.capitalize())
+
+@app.route('/download')
+def download():
+    image = request.args['image'] 
+    
+    return send_file("static/" + image, as_attachment=True)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- image.py
+++ image.py
@@ -3,33 +3,35 @@ import sys
 
 # listofname = ["Vuthy vey", "Reaksmy Dancer", "Visal Sao", "mengthong LONg","រំដួល"]
 # # img = Image.new('RGB', (100, 30), color = (73, 109, 137))
-def createTicket(name, nationality, id):
+def createTicket(name, nationality, order):
     img = Image.open("test.png")
-    id = str(   id)
-    for x in range(6 - len(id)):
-        id = "0"+ id
+    order = str(order)
+    order1 = order
+    for x in range(6 - len(order)):
+        order = "0"+ order
     d = ImageDraw.Draw(img)
     fontsize = 50
     font = ImageFont.truetype("Myriad Pro Regular.ttf", fontsize)
-    d.text((545,270), name, font=font, fill=(0,0,0))
-    d.text((1090,270), nationality, font=font, fill=(0,0,0))
-    d.text((1570,130), id, font=font, fill=(0,0,0))
+    d.text((560,257), name, font=font, fill=(227,69,69))
+    d.text((1170,257), nationality, font=font, fill=(227,69,69))
+    d.text((1620,130), order, font=font, fill=(255,255,255))
     # d.text((1600,270), name, font=font, fill=(0,0,0))
-    
+  
     txt=Image.new('L', (500,50))
     textName = ImageDraw.Draw(txt)
-    textName.text( (0, 0), name,  font=font, fill=255)
+    textName.text( (0, 0), name,  font=font, fill=(255))
     w=txt.rotate(-90,  expand=1)
-    img.paste( ImageOps.colorize(w, (0,0,0), (0,0,0)), (1865,100),  w)
-    
+    # img.paste( ImageOps.colorize(w, (200,0,0), (0,0,0)), (1865,100),  w)
+    img.paste((255,255,255), box=(1855,100), mask=w) 
+
     txt=Image.new('L', (500,50))
-    textID = ImageDraw.Draw(txt)
-    textID.text( (0, 0), id,  font=font, fill=255)
+    textorder = ImageDraw.Draw(txt)
+    textorder.text( (0, 0), order,  font=font, fill=255)
     w=txt.rotate(-90,  expand=1)
-    img.paste( ImageOps.colorize(w, (0,0,0), (0,0,0)), (1865,585),  w)
-    
-    img.save("static/" + name + ".png")
-    return name + ".png"
+    # img.paste( ImageOps.colorize(w, (0,0,0), (0,0,0)), (1865,585),  w)
+    img.paste((255,255,255), box=(1855,585), mask=w) 
+        
+    img.save("static/" + name + order1 + ".png")
+    return name + order1 + ".png"
 
 sys.modules[__name__] = createTicket
-
